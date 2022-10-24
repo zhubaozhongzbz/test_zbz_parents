fd9bae01c31d42758c565692d52d1276253a59a3
zbz <289292245@qq.com>
2022-10-24 11:50:12 +0800

    new 1

--- test1_zbz_module1/src/main/java/com/zbz/HelloWorld.java
+++ test1_zbz_module1/src/main/java/com/zbz/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public String say() {
            String s = new String();
 
-        String s1 = new String();
+
         String s4 = new String();
         String s3 = new String();
         String s2 = new String();
