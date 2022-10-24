c06e01efafde079e3a1929f3690598cfc58fbe80
zbz <289292245@qq.com>
2022-10-24 10:56:56 +0800

    new

---
+++ test1_zbz_module1/src/main/java/com/zbz/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zbz;
+
+public class HelloWorld {
+
+    public String say() {
+           String s = new String();
+
+        String s1 = new String();
+        System.out.println("say holloworld");
+        return "say";
+    }
+}
---
+++ test1_zbz_module1/src/test/java/com/zbz/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package com.zbz;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+    @Test
+    public void testSay(){
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.say();
+
+
+    }
+
+}
