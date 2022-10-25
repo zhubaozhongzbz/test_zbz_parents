0a5f8932609fff542f1196408b0e16cb7f056771
zbz <289292245@qq.com>
2022-10-25 10:21:04 +0800

    java8

--- test1_zbz_module1/src/main/java/com/zbz/HelloWorld.java
+++ test1_zbz_module1/src/main/java/com/zbz/HelloWorld.java
@@ -4,12 +4,13 @@ public class HelloWorld {
 
     public String say() {
            String s = new String();
-
+        System.out.println("qqqqqqqqqqqqqqq");
 
 
         String s3 = new String();
         String s2 = new String();
         System.out.println("say holloworld");
         return "say";
+
     }
 }
